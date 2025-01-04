cfe6bef50d48e5f0e5117cbdee611ee2e96ef901
Rohit <149556468+rohit-shivhare@users.noreply.github.com>
2025-01-04 14:48:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+// Sticky Navigation Menu
+let nav = document.querySelector("nav");
+let scrollBtn = document.querySelector(".scroll-button a");
+
+// Show/hide sticky navigation and scroll button based on scroll position
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 20) {
+        nav.classList.add("sticky");
+        scrollBtn.style.display = "block";
+    } else {
+        nav.classList.remove("sticky");
+        scrollBtn.style.display = "none";
+    }
+};
+
+// Side Navigation Menu
+let body = document.querySelector("body");
+let navBar = document.querySelector(".navbar");
+let menuBtn = document.querySelector(".menu-btn");
+let cancelBtn = document.querySelector(".cancel-btn");
+
+// Open side navigation
+menuBtn.onclick = function () {
+    navBar.classList.add("active");
+    menuBtn.style.opacity = "0";
+    menuBtn.style.pointerEvents = "none";
+    body.style.overflow = "hidden";
+    scrollBtn.style.pointerEvents = "none";
+};
+
+const hideNavMenu = () => {
+    navBar.classList.remove("active");
+    menuBtn.style.opacity = "1";
+    menuBtn.style.pointerEvents = "auto";
+    body.style.overflow = "auto";
+    scrollBtn.style.pointerEvents = "auto";
+};
+
+// Close side navigation
+cancelBtn.onclick = hideNavMenu;
+
+// Close side navigation when a menu link is clicked
+let navLinks = document.querySelectorAll(".menu li a");
+navLinks.forEach((link) => {
+    link.addEventListener("click", hideNavMenu);
+});
+
+// Attatch Resume file
+document.getElementById('download-btn').addEventListener('click', function () {
+    const link = document.createElement('a');
+    link.href = 'https://drive.google.com/file/d/1P7cmTSrhwgzQdq4l_nIg0ebvqRA3lwSI/view?usp=sharing'; 
+    link.target = '_blank';
+    link.click();
+});
+
